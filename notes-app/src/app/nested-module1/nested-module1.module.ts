f04506f0c7966c8cbf3ee49732520a37e945740b
Goia Ciprian <goia.ciprian14@gmail.com>
2022-03-03 14:06:16 +0200

    modificat exporturile din module pentru tema curs 2

--- notes-app/src/app/nested-module1/nested-module1.module.ts
+++ notes-app/src/app/nested-module1/nested-module1.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NestedModule2Module } from '../nested-module2/nested-module2.module';
-import { Nested3Component } from '../nested3/nested3.component';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, NestedModule2Module],
-  exports: [Nested3Component],
+  exports: [NestedModule2Module],
 })
 export class NestedModule1Module {}
--- notes-app/src/app/nested-module2/nested-module2.module.ts
+++ notes-app/src/app/nested-module2/nested-module2.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NestedModule3Module } from '../nested-module3/nested-module3.module';
-import { Nested3Component } from '../nested3/nested3.component';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, NestedModule3Module],
-  exports: [Nested3Component],
+  exports: [NestedModule3Module],
 })
 export class NestedModule2Module {}
